fe28fc2554c177b1ab4343cdb41674a6cefd8b7e
Mika Ridley <mikaridley26@gmail.com>
2025-11-12 10:39:59 +0200

    Add meme with text

---
+++ js/meme-controller.js
@@ -0,0 +1,48 @@
+'use strict'
+var gElCanvas
+var gCtx
+
+function onInit() {
+  renderMeme()
+}
+
+function renderMeme(img) {
+  renderCanvas()
+  const elImg = new Image()
+  elImg.src = 'img/1.jpg'
+  elImg.onload = () => {
+    gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+    drawText('Insert Text')
+  }
+}
+
+function renderCanvas() {
+  gElCanvas = document.querySelector('canvas')
+  gCtx = gElCanvas.getContext('2d')
+
+  //4:3 ratio
+  gElCanvas.width = window.innerWidth * 0.6
+  gElCanvas.height = gElCanvas.width * 0.75
+
+  gCtx.fillStyle = '#f4f4f4'
+  gCtx.fillRect(0, 0, gElCanvas.width, gElCanvas.height)
+}
+
+function drawText(txt) {
+  const fontSize = gElCanvas.height * 0.08
+  gCtx.font = `${fontSize}px Impact`
+  gCtx.textAlign = 'center'
+  gCtx.textBaseline = 'top'
+
+  gCtx.lineWidth = fontSize / 20
+
+  gCtx.strokeStyle = 'black'
+  gCtx.fillStyle = 'white'
+
+  //put the txt constant
+  const x = gElCanvas.width / 2
+  const y = gElCanvas.height * 0.1
+
+  gCtx.fillText(txt, x, y)
+  gCtx.strokeText(txt, x, y)
+}
